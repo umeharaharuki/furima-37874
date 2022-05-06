3d4e1273611bb462366caf18823c714ac1448312
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-06 15:04:39 +0900

    バリデーション記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  with_options presence: true do
+  validates :nickname
+  validates :first_name
+  validates :last_name
+  validates :first_name_kana
+  validates :last_name_kana
+  validates :birthday
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+end
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
