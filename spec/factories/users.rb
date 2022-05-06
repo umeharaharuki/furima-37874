117d9d741840b104ed275be31073876fe2d7707d
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-06 20:58:58 +0900

    関連Gemの導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {Faker::Internet.free_email}
+    password              {'0a0a0a'}
+    password_confirmation {password}
+    first_name            {'太郎'}
+    last_name             {'田中'}
+    first_name_kana       {'タロウ'}
+    last_name_kana        {'タナカ'}
+    birthday              {'1931-03-03'}
+  end
+end
