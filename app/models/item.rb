06aaf9b0391752e02bf22cb7a96cd721ddd6942a
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-08 16:31:41 +0900

    itemsテーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :product_name
+    validates :explanation
+    validates :category_id
+    validates :situation_id
+    validates :last_name_kana
+    validates :birthday
 end
