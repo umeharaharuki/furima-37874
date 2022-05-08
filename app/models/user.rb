e67a777fc002f8bc52ecc3004f5d5f0c2ee1e0dd
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-08 14:52:20 +0900

    重複するため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :birthday
 
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :first_name
       validates :last_name
     end
 
-    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
       validates :first_name_kana
       validates :last_name_kana
     end
