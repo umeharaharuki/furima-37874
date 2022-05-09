4c630c28ce47cfde86cab2b43740afc9b9f1faca
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-09 20:13:51 +0900

    JavaScriptの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name,:explanation,:category_id,:situation_id,:charge_id,:prefecture_id,:ship_id,:price,:image).merge(user_id: current_user.id)
   end
+
+
 end
