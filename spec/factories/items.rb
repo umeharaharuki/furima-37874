33c6a54f3a904d1f7f7327038b9c584f03026ec9
umeharaharuki <jasper1116lopez@icloud.com>
2022-05-10 14:27:36 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :item do
-    nickname              { 'test' }
-    email                 { Faker::Internet.free_email }
-    password              { '0a0a0a' }
-    password_confirmation { password }
-    first_name            { '太郎' }
-    last_name             { '田中' }
-    first_name_kana       { 'タロウ' }
-    last_name_kana        { 'タナカ' }
-    birthday              { '1931-03-03' }
+    product_name  { 'aaaaa' }
+    explanation   { 'aaaaa' }
+    category_id   { '2' }
+    situation_id  { '2' }
+    charge_id     { '2' }
+    prefecture_id { '2' }
+    ship_id       { '2' }
+    price         { '350' }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
